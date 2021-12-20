c2485661e423d2beccc0be565fdfa5eb19884c37
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-12-20 15:46:36 -0300

    topbar component

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -3,7 +3,23 @@ import "./topbar.css";
 function TopBar() {
     return (
         <div className='top'>
-            topbar
+            <div className="topLeft">
+                <i className="topIcon fab fa-facebook-square"></i>
+                <i className="topIcon fab fa-linkedin"></i>
+                <i className="topIcon fab fa-github-square"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <img className="topImage" src="https://picsum.photos/200/300?random=2" alt="profile-img" />
+                <i className="topSearchIcon fas fa-search"></i>
+            </div>
         </div>
     )
 }
