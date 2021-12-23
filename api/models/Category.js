207235fb41a8276d381da2a52daff9c4297fcb4f
EstebanToloza <estebanisaiastoloza@gmail.com>
2021-12-23 12:39:20 -0300

    routes and models, done

--- api/models/Category.js
+++ api/models/Category.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-const CategorySchema = new mongoose.Schema({
+const CategorySchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
-    }
-}, { timestamps: true } );
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Category", CategorySchema);
